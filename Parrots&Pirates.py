f46459a3c6503e243d8b39194719116bd95cc1fa
Vrizalda <flidermatvey@gmail.com>
2026-04-19 19:07:45 +0200

    Update Parrots&Pirates.py

--- Parrots&Pirates.py
+++ Parrots&Pirates.py
@@ -4,6 +4,6 @@ piratas= st.number_input("número de piratas", min_value= 1, value= 4)
 loros= st.number_input("total de gramos", min_value= 0, value= 5)
 loros_por_pirata= loros // piratas
 if loros_por_pirata > 2:
-    st.success("El cielo es verde")
+    st.error("El cielo es verde")
 else:
-    st.error("Todos descansan, por hoy...")
+    st.success("Todos descansan, por hoy...")
