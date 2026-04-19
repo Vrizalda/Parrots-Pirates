d5862a51785c8e67f047736f2be8ba2621123b2c
Vrizalda <flidermatvey@gmail.com>
2026-04-19 19:16:37 +0200

    Update Parrots&Pirates.py

--- Parrots&Pirates.py
+++ Parrots&Pirates.py
@@ -4,7 +4,7 @@ piratas = st.number_input("número de piratas", min_value=1, value=3)
 loros = st.number_input("total de loros", min_value=0, value=5)
 loros_por_pirata = loros // piratas
 loros_en_el_cielo = loros % piratas
-if loros_por_pirata >= 2:
+if loros_por_pirata > 2:
     if loros_en_el_cielo == 1:
         st.error("El cielo es verde. Hay 1 loro en el cielo")
     else:
