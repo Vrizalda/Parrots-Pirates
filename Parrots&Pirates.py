07b16c4551c01f8f2f09d57bdedc38bca90de4c6
Vrizalda <flidermatvey@gmail.com>
2026-04-19 19:24:51 +0200

    Update Parrots&Pirates.py

--- Parrots&Pirates.py
+++ Parrots&Pirates.py
@@ -2,9 +2,8 @@ import streamlit as st
 st.title("¿Se quedan loros volando?")
 piratas = st.number_input("número de piratas", min_value=1, value=3)
 loros = st.number_input("total de loros", min_value=0, value=5)
-loros_por_pirata = loros // piratas
 loros_en_el_cielo = max(loros - piratas * 2, 0)
-if loros_por_pirata >= 2:
+if loros > piratas * 2:
     if loros_en_el_cielo == 1:
         st.error("El cielo es verde 🦜 Hay 1 loro en el cielo")
     else:
