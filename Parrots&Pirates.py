0d32d1a6f5b27fa774418e23a261025d0b602fa5
Vrizalda <flidermatvey@gmail.com>
2026-04-19 19:08:15 +0200

    Update Parrots&Pirates.py

--- Parrots&Pirates.py
+++ Parrots&Pirates.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.title("¿Se quedan loros volando?")
 piratas= st.number_input("número de piratas", min_value= 1, value= 4)
-loros= st.number_input("total de gramos", min_value= 0, value= 5)
+loros= st.number_input("total de loros", min_value= 0, value= 5)
 loros_por_pirata= loros // piratas
 if loros_por_pirata > 2:
     st.error("El cielo es verde")
